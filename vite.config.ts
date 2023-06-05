adefa04f170543f0690204c83ba5714836cebf36
BOB <vasiasko112@gmail.com>
2023-06-05 16:58:19 +0400

    none

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/Spaguletti_IceCream',
+  base:'/Spaguletti',
   plugins: [vue({
     template: {
       compilerOptions: {
