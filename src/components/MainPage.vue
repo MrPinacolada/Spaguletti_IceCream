2a77fb02fcadb7fdca4b8eea5463daffd2eb42a3
BOB <vasiasko112@gmail.com>
2023-06-05 16:17:56 +0400

    none

---
+++ src/components/MainPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. At adipisci in tempore? Repudiandae
+      dignissimos doloribus, a voluptatem minima consequatur eos.
+    </p>
+  </div>
+</template>
+<script lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import { defineComponent, ref, watch } from 'vue'
+export default defineComponent({
+  components: {},
+  setup() {
+    return {}
+  }
+})
+</script>
+<style scoped></style>
