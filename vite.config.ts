14f64af97e3e0e15809f40d609b5c0eeb242f36c
BOB <vasiasko112@gmail.com>
2023-06-05 16:46:38 +0400

    none

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/Spaguletti',
+  base:'/Spaguletti_IceCream',
   plugins: [vue({
     template: {
       compilerOptions: {
