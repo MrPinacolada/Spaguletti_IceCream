2bc72199ec34e5c074dd269c38b911f0f8875e96
BOB <vasiasko112@gmail.com>
2023-06-07 18:55:04 +0400

    none

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -210,21 +210,21 @@ export default defineComponent({
     let mesMessage = ref()
     let iceCreamFlavorsinView = ref()
     let flavours = [
-      '@/assets/Body/gridicetable/chocolate.svg',
-      '@/assets/Body/gridicetable/honey.svg',
-      '@/assets/Body/gridicetable/mint.svg',
-      '@/assets/Body/gridicetable/chocolate.svg',
-      '@/assets/Body/gridicetable/honey.svg',
-      '@/assets/Body/gridicetable/mint.svg',
-      '@/assets/Body/gridicetable/chocolate.svg',
-      '@/assets/Body/gridicetable/honey.svg',
-      '@/assets/Body/gridicetable/mint.svg',
-      '@/assets/Body/gridicetable/chocolate.svg',
-      '@/assets/Body/gridicetable/honey.svg',
-      '@/assets/Body/gridicetable/mint.svg',
-      '@/assets/Body/gridicetable/chocolate.svg',
-      '@/assets/Body/gridicetable/honey.svg',
-      '@/assets/Body/gridicetable/mint.svg'
+      '/assets/Body/gridicetable/chocolate.svg',
+      '/assets/Body/gridicetable/honey.svg',
+      '/assets/Body/gridicetable/mint.svg',
+      '/assets/Body/gridicetable/chocolate.svg',
+      '/assets/Body/gridicetable/honey.svg',
+      '/assets/Body/gridicetable/mint.svg',
+      '/assets/Body/gridicetable/chocolate.svg',
+      '/assets/Body/gridicetable/honey.svg',
+      '/assets/Body/gridicetable/mint.svg',
+      '/assets/Body/gridicetable/chocolate.svg',
+      '/assets/Body/gridicetable/honey.svg',
+      '/assets/Body/gridicetable/mint.svg',
+      '/assets/Body/gridicetable/chocolate.svg',
+      '/assets/Body/gridicetable/honey.svg',
+      '/assets/Body/gridicetable/mint.svg'
     ]
     let onIntersectionObserverIceCups = ([{ isIntersecting }]: any) => {
       if (isIntersecting) {
