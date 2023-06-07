66afc246afaf7f8a245069f32e5cd19ff630def5
BOB <vasiasko112@gmail.com>
2023-06-07 19:43:56 +0400

    none

--- vite.config.ts
+++ vite.config.ts
@@ -2,52 +2,42 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import path from "path";
-
-// https://vitejs.dev/config/
+import path from 'path'
 export default defineConfig({
-  base:'/Spaguletti_IceCream',
-  plugins: [vue({
-    template: {
-      compilerOptions: {
-        isCustomElement: (tag) => ["lottie-player"].includes(tag),
-      },
-    },
-  }), vueJsx()],
+  base: '/Spaguletti_IceCream',
+  plugins: [
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: (tag) => ['lottie-player'].includes(tag)
+        }
+      }
+    }),
+
+    vueJsx()
+  ],
   resolve: {
     alias: [
       {
-        find: "@",
-        replacement: path.resolve(__dirname, "src"),
+        find: '@',
+        replacement: path.resolve(__dirname, 'src')
       },
       {
-        find: "@SwiperBundleCss",
-        replacement: path.resolve(
-          __dirname,
-          "./node_modules/swiper/swiper-bundle.min.css"
-        ),
+        find: '@SwiperBundleCss',
+        replacement: path.resolve(__dirname, './node_modules/swiper/swiper-bundle.min.css')
       },
       {
-        find: "@SwiperBundle",
-        replacement: path.resolve(
-          __dirname,
-          "./node_modules/swiper/swiper-bundle.esm.js"
-        ),
+        find: '@SwiperBundle',
+        replacement: path.resolve(__dirname, './node_modules/swiper/swiper-bundle.esm.js')
       },
       {
-        find: "@Swiper",
-        replacement: path.resolve(
-          __dirname,
-          "./node_modules/swiper/swiper.esm.js"
-        ),
+        find: '@Swiper',
+        replacement: path.resolve(__dirname, './node_modules/swiper/swiper.esm.js')
       },
       {
-        find: "@SwiperVue",
-        replacement: path.resolve(
-          __dirname,
-          "./node_modules/swiper/vue/swiper-vue.js"
-        ),
-      },
-    ],
-  },
+        find: '@SwiperVue',
+        replacement: path.resolve(__dirname, './node_modules/swiper/vue/swiper-vue.js')
+      }
+    ]
+  }
 })
